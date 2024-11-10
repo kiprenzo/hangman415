e31636042a5df63e81671380cef3c3fdf8019e45
kiprenzo <kipras.varanavicius@gmail.com>
2024-11-10 12:02:31 +0000

    Introduces the play() function, guess validation, and Hangman display().

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Hangman
+# Moody Hangman
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
+In this project I want to try and see whether any level of animation is possible in Python and test my basic coding skills.
--- milestone_2.py
+++ milestone_2.py
@@ -1,20 +1,55 @@
-word_list = ['jackfruit', 'strawberry', 'mango', 'peach', 'nectarine']
-
-print(word_list)
-
 import random as r
 
-word = r.choice(word_list)
+from display_hangman import display
+
+# WIP
+word_list = ['jackfruit', 'strawberry', 'mango', 'peach', 'nectarine']
+
+
+# This section contains lists of phrases that add some flavour to the prompts.
+game_word = r.choice(word_list)
 input_prompt = r.choice(["Enter a letter, chief. ","What do you think...? ", "What have we got, then? ", "What's up next? ", "Go ahead... "])
-good_guess = r.choice(["That will do.","I suppose...", "Fine.", "Let's see here..."])
-bad_guess = r.choice(["Not sure about that one. Again. ", "No. That won't work. ", "I don't think so, chief. Try again. ", "Nu-uh."])
+good_guess = r.choice(["That will do. ","I suppose... ", "Fine. ", "Let's see here... "])
+correct_guess = r.choice(["Very good... ", "You live yet... ", "Interesting... ", "Hmm. You got one. "])
+wrong_guess = r.choice(["Afraid it's not in the word... ", "Ahh, so close. ", "Not this one... ", "Ouch. No. "])
+bad_guess = (["Not sure that's one letter...", "No. That won't work. Guess one letter. ", "I don't think so, chief. Try a single letter. ", "Nu-uh."])
+lets_play = r.choice(["Are you ready then?", "Let's try your luck.", "Back again, are we...", "Let's see how you get on this time..."])
+
+print(game_word.upper())
+
+def user_get_letter_guess():
+    while True:
+        guess = input(input_prompt)
+        if is_valid_guess(guess) == True:
+            print(good_guess)
+            return guess.upper()
+
+        else:
+            print(r.choice(bad_guess))
+    
+
+def is_valid_guess(guess):
+    if len(guess) == 1 and guess.isalpha():
+        return True
+    else:
+        return False
+        
+def guess_in_word(guess):
+    if guess in game_word:
+        pass
 
 
-print(word)
 
-guess = input(input_prompt)
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print(lets_play)
+    print(display(tries))
+    print (f"\n\t\t{word_completion}\n")
 
-if len(guess) == 1 and guess.isalpha() == True:
-    print(good_guess)
-else:
-    print(bad_guess)
+
+play('falafel')
+user_get_letter_guess()
