1ca686ac9c49a9d010ffbebf4391fdfc5c70e393
kiprenzo <kipras.varanavicius@gmail.com>
2024-11-11 14:20:25 +0000

    Adds Hangman class and includes all functions within the class; breaks up play() to include a guess_in_word() check to improve code legibility.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Moody Hangman
-Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
+# Sassy Hangman
 
-This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
-In this project I want to try and see whether any level of animation is possible in Python and test my basic coding skills.
+![preview](images/gui.jpg)
+
+In one of my first Python projects, I wanted to test my understanding of the language basics by building a Hangman game.
+
+To add a bit of flair, I've added randomized lines for the... executioner, if you will. 
+
+### Requirements
+You will find this on the light side, with the only thing needed to run the game being the 'requests' library.
+
+Run: `pip install requests` to install.
+
+If this doesn't work, try replacing `pip` with `pip3`.
--- milestone_2.py
+++ milestone_2.py
@@ -10,105 +10,105 @@ word_list = response.text.splitlines()
 long_words = [word for word in word_list if len(word) > 2]
 
 
-game_word = r.choice(long_words).upper()
+# game_word = r.choice(long_words).upper()
 
 # This section contains lists of phrases that add some sassy flavour to the prompts.
 # These get called using random.choice() in the functions below.
 input_prompt = (["Enter a letter, chief. ","What do you think...? ", "What have we got, then? ", "What's up next? ", "Go ahead... "])
 valid_guess = (["That will do. ","I suppose... ", "Fine. ", "Let's see here... "])
-# Whereas correct means the letter is in the word.
 correct_guess = (["Very good... ", "You live yet... ", "Interesting... ", "Hmm. You got one. "])
-
 wrong_guess = (["Afraid it's not in the word... ", "Ahh, so close. ", "Not this one... ", "Ouch. No. "])
-
-
 invalid_guess = (["Not sure that's one letter...", "No. That won't work. Guess one letter. ", "I don't think so, chief. Try a single letter. ", "Nu-uh."])
-
 lets_play = (["Are you ready then?", "Let's try your luck.", "Back again, are we...", "Let's see how you get on this time..."])
 lost_game = (["Too bad, bucko. You'll get 'em next time.", "How unfortunate. Maybe next time.", "Well well. You gave it your best. Perhaps you'll try again.", "So close. Better luck next time."])
 won_game = (["Looky here, you actually pulled it off.", "Colour me impressed. Well played.", "That's the one... well done. Another?"])
 
 
-def user_get_letter_guess():
-    while True:
-        guess = input(r.choice(input_prompt))
-        if is_valid_guess(guess) == True:
-            # print(r.choice(valid_guess)) - redundant
-            return guess.upper()
+class Hangman:
+    def __init__(self, game_word: str):
+        self.game_word = game_word.upper()
+        self.word_completion = "_" * len(self.game_word)
+        self.guessed = False
+        self.guessed_letters = []
+        # self.guessed_words = []
+        self.attempts_left = 6
 
+    def is_valid_guess(self, guess):
+        if len(guess) == 1 and guess.isalpha():
+            return True
         else:
-            print(r.choice(invalid_guess))
-    
-
-def is_valid_guess(guess):
-    if len(guess) == 1 and guess.isalpha():
-        return True
-    else:
-        return False
+            return False
         
-def guess_in_word(guess):
-    if guess in game_word:
-        return True
-    else:
-        return False
+    def user_get_letter_guess(self):
+        while True:
+            guess = input(r.choice(input_prompt))
+            if self.is_valid_guess(guess) == True:
+                # print(r.choice(valid_guess)) - redundant
+                return guess.upper()
 
-
-
-def play(word):
-    # This function contains the whole logic for the Hangman game.
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    attempts_left = 6
-    print(r.choice(lets_play))
-    print(display(attempts_left))
-    print (f"\n\t\t{word_completion}\n")
-    while not guessed and attempts_left > 0:
-        guess = user_get_letter_guess()
-        if guess in guessed_letters:
-            print(f"You already tried {guess}.")
-        elif guess not in word:
+            else:
+                print(r.choice(invalid_guess))
+            
+    def guess_in_word(self, guess):
+        if guess not in self.game_word:
             print(r.choice(wrong_guess))
-            attempts_left -= 1
-            guessed_letters.append(guess)
-            print(display(attempts_left))
-            print (f"\n\t\t{word_completion}\n")
-            print(f"\n\tAlready guessed: {[l for l in guessed_letters if l not in word]}\n")
-# TODO maybe:
-# add elif for guessing words
+            self.attempts_left -= 1
+            self.guessed_letters.append(guess)
+            print(display(self.attempts_left))
+            print (f"\n\t\t{self.word_completion}\n")
+            print(f"\n\tAlready guessed: {[l for l in self.guessed_letters if l not in self.game_word]}\n")
         else:
-            print(r.choice(correct_guess))
-            guessed_letters.append(guess)
-            word_as_list = list(word_completion)
-            indices = [i for i, letter in enumerate(word) if letter == guess]
-            for index in indices:
-                word_as_list[index] = guess
-            word_completion = "".join(word_as_list)
-            print(display(attempts_left))
-            print (f"\n\t\t{word_completion}\n")
-            print(f"\n\tAlready guessed: {[l for l in guessed_letters if l not in word]}\n")
-            if "_" not in word_completion:
-                guessed = True
-    if attempts_left == 0:
-        word_completion = word
-        print(display(attempts_left))
-        print (f"\n\t\t{word_completion}\n")
-        print(r.choice(lost_game))
-    if guessed == True:
-        print(f"\n{r.choice(won_game)}")
+                print(r.choice(correct_guess))
+                self.guessed_letters.append(guess)
+                word_as_list = list(self.word_completion)
+                indices = [i for i, letter in enumerate(self.game_word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                self.word_completion = "".join(word_as_list)
+                print(display(self.attempts_left))
+                print (f"\n\t\t{self.word_completion}\n")
+                print(f"\n\tAlready guessed: {[l for l in self.guessed_letters if l not in self.game_word]}\n")
+        
+
+
+
+    def play(self):
+        # This function contains the whole logic for the Hangman game.
+
+        print(r.choice(lets_play))
+        print(display(self.attempts_left))
+        print (f"\n\t\t{self.word_completion}\n")
+        while not self.guessed and self.attempts_left > 0:
+            guess = self.user_get_letter_guess()
+            if guess in self.guessed_letters:
+                print(f"You already tried {guess}.")
+            self.guess_in_word(guess)
+
+            if "_" not in self.word_completion:
+                    self.guessed = True
+
+        if self.attempts_left == 0:
+            self.word_completion = self.game_word
+            print(display(self.attempts_left))
+            print (f"\n\t\t{self.word_completion}\n")
+            print(r.choice(lost_game))
+        if self.guessed == True:
+            print(f"\n{r.choice(won_game)}")
             
 
 
 def main():
-    word = r.choice(long_words).upper()
-    play(word)
+    game_word = r.choice(long_words).upper()
+    game = Hangman(game_word)  # Create an instance of Hangman with game_word
+    game.play()  
     while input("Play again? (Y/N) ").upper() == "Y":
-        word = r.choice(long_words).upper()
-        play(word)
+        game_word = r.choice(long_words).upper()
+        game = Hangman(game_word)  # Create a new instance for each new game
+        game.play()
 
 if __name__ == "__main__":
     main()
+
         
 
 
